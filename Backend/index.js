b6dc94ba69fdc6bbb2ce212d6edb9c5aad93abff
ShreshthVyas <shreshth.vyas.2000@gmail.com>
2024-04-22 15:20:09 +0530

    final

--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,7 @@ const faqData = require('./faqData.json'); // Import your FAQ data
 const app = express();
 const PORT = process.env.PORT || 3000;
 const fs = require('fs');
-
+const path = require('path');
 
 app.use(cors());
 app.use(express.json());
